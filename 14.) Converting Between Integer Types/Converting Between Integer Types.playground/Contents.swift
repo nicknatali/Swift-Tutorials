859c805881316d44cfe421b4dd5c4dcce58c9024
Nickolas Natali <nickolasnatali@gmail.com>
2017-05-01 10:55:43 -0700

    Add files via upload

---
+++ 14.) Converting Between Integer Types/Converting Between Integer Types.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let yourMomma: Int16 = 200
+let yourDaddy: Int8 = 50
+let theChild = yourMomma + Int16(yourDaddy) //A beautiful baby
+
