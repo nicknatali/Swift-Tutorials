8b3575624e5299b43d9dacca0cf6393540c091b7
Nickolas Natali <nickolasnatali@gmail.com>
2017-05-01 10:51:13 -0700

    Add files via upload

---
+++ 12.) Operator Shorthand/Operator Shorthand.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var x = 10
+x += 10 //This is the equivalent to : x = x + 1-
+print("x has added 10 to it and is now \(x)")
+
+x -= 5 //Equivalent to: x = x - 5
+print("x has subtracted 5 from it and is now \(x)")
+
+
