a0ac1ba14cb13fc5af6d9cb2efae6b5961837e3e
Nickolas Natali <nickolasnatali@gmail.com>
2017-05-01 10:48:53 -0700

    Add files via upload

---
+++ 6.) If Else/Conditionals.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var familySize: Int = 5
+var message: String
+var isACoolGuy: Bool = true
+var hasCats: Bool = true
+var age: Int = 21
+
+if familySize < 4 {
+    message = "\(familySize) is a pretty small family!"
+} else {
+    message = "\(familySize) is a pretty big family!"
+}
+
+
+if !isACoolGuy {
+    print("Don't let him date your daughter")
+}
+
+
+if !hasCats || age < 21 {
+    print("Date her")
+} else {
+    print("dump her")
+}
+
