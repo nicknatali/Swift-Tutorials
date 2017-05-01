1705cf864034b519223c0960df1af3e3c1a98b9b
Nickolas Natali <nickolasnatali@gmail.com>
2017-05-01 10:48:24 -0700

    Add files via upload

---
+++ 5.) String Interpolation/String Interpolation.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStores = 6
+var cityPopulation: Int
+cityPopulation = 4000
+let cityName: String = "Jerusalem"
+
+let cityDescription = "\(cityName) has a population of \(cityPopulation) and \(numberOfStores) stores. And is also Jesus's hometown"
+
+print(cityDescription)
+
+
+
+
+
+
+
+
+
+
+
+
