9ebde9e6e632dc3054f5ec8ff0fc8d1a88d4c2ad
Nickolas Natali <nickolasnatali@gmail.com>
2017-05-01 10:50:12 -0700

    Add files via upload

---
+++ 9.) Numbers/Numbers.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+
+print("The maximum value for an Int is \(Int.max)")
+print("The minimum Int value is \(Int.min)")
+
+
+print("The maximum value for a 32-bit Int is \(Int16.max)")
+print("The minimum Int 32-bit value is \(Int32.min)")
