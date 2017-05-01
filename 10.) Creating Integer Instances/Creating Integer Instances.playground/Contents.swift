bfa8ce279c408d45c10c9b373fb570a586eea6a7
Nickolas Natali <nickolasnatali@gmail.com>
2017-05-01 10:50:34 -0700

    Add files via upload

---
+++ 10.) Creating Integer Instances/Creating Integer Instances.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//Declare the type explicitly
+let amountOfGains: Int = 10
+
+//Also of type Int, but inferred by the compiler 
+let numberOfBodyBuilders = 3
+
+
+let numOfSkateboards: UInt = 8
+
+let numOfCrossfitGains: Int32 = -1000
+
+
+
